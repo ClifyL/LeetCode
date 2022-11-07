62add9c822cd7963b8f4c9ad2676febcb654557c
clify <licux090@gmail.com>
2022-11-07 15:39:00 +0800

    最大回文字串

---
+++ LeetCode5/main/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(findMedianSortedArrays([]int{1, 2, 3, 4}, []int{1, 2, 3, 4}))
+}
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var (
+		pointOne = 0
+		pointTwo = 0
+	)
+
+	list := make([]int, len(nums1)+len(nums2))
+	for i := 0; i < len(nums1)+len(nums2); i++ {
+
+		if pointOne >= len(nums1) {
+			list[i] = nums2[pointTwo]
+			pointTwo++
+			continue
+		}
+		if pointTwo >= len(nums2) {
+			list[i] = nums1[pointOne]
+			pointOne++
+			continue
+		}
+		if nums1[pointOne] < nums2[pointTwo] {
+			list[i] = nums1[pointOne]
+			pointOne++
+		} else {
+			list[i] = nums2[pointTwo]
+			pointTwo++
+		}
+	}
+	return float64(list[int((len(nums1)+len(nums2)+1)/2)-1]+list[int((len(nums1)+len(nums2)+2)/2)-1]) / 2
+}
